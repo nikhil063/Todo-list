3b12dc0674fd1beea105fa3483f3d58eba4a5079
nikhil063 <122106278+nikhil063@users.noreply.github.com>
2023-03-29 12:14:14 +0530

    Update App.js

--- App.js
+++ App.js
@@ -1,34 +1,3 @@
-// import React,{useState} from 'react';
-// import Todo from './Todo';
-// import TodoForm from './TodoForm'
-
-// function App (){
-
-//     const [todos, setTodos] = useState([]);
-  
-//     const addTodo = (todo) => {
-//       setTodos([...todos,todo])
-//     };
-  
-//     const removeTodo = (id) => {
-//       setTodos([...todos].filter((todo) => todo.id !== id));
-//     };
-  
-//     const updateTodo = (todoId, newValue) => {
-//       setTodos((prev) => prev.map((item) => (item.id === todoId ? newValue : item)));
-//     };
- 
-//   return (
-//     <>
-//       <h1>To do list</h1>
-//       <TodoForm onSubmit={addTodo} />
-//       <Todo todos={todos} removeTodo={removeTodo} updateTodo={updateTodo} />
-//     </>
-//   );
-// }
-
-// export default App;
-
 import { useState } from "react";
 import Listing from "./Listing";
 
