4d1e7abc04d6aeac8c8c839b09642df97d26f3b4
nikhil063 <122106278+nikhil063@users.noreply.github.com>
2023-03-29 12:21:27 +0530

    Update App.js

--- App.js
+++ App.js
@@ -1,16 +1,15 @@
+import "./styles.css";
 import { useState } from "react";
-import Listing from "./Listing";
-
+import Listing from "./components/Listing";
+// import Listing2 from "./components/Listing2";
 export default function App() {
   const [data, setData] = useState([]);
   const [name, setName] = useState();
-
   const handleSubmit = () => {
     let temp = name;
     setData([...data, temp]);
     setName("");
   };
-
   console.log("data", data);
   return (
     <div className="App">
@@ -27,6 +26,3 @@ export default function App() {
     </div>
   );
 }
-
-
-
